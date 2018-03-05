3572d3c5fca5df91f841fa245283881a541cb18b
Christopher Bravo <bravo.c@husky.neu.edu>
2018-03-05 11:15:26 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 // ... it's a good time!
 
 function main () {
+  $('title').value = "Christopher Bravo" // overwriting title so it just says my name and not some silly title, hopefully
   $('p').hide();
   $('p').fadeIn(1000);
 }
