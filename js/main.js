b107ed91487a0b2a9fe5f2f9df844cf890219cd3
Christopher Bravo <bravo.c@husky.neu.edu>
2018-03-05 11:24:44 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
-// messing about with jQuery
-// ... it's a good time!
-
-function main () {
+// The main function is now a self-invoking document ready function
+$(function() {
   document.title = "Christopher Bravo" // overwriting title so it just says my name and not some silly title, hopefully
   $('p').hide();
   $('p').fadeIn(1000);
-}
-
-$(document).ready(main);
+  $('a.logoLink').value = "Christopher Bravo"
+}); // end of "main" / document ready function
