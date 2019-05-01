14704cf73c3eb2b77647d4c4a6d1bf047c2b5b75
Christopher Bravo <bravo.c@husky.neu.edu>
2019-04-30 22:35:46 -0400

    doing a huge revamp woo

--- old_structure/js/main.js
+++ old_structure/js/main.js
@@ -1,44 +1,20 @@
-// The main function is now a self-invoking document ready function
-$(function() {
-  // let's just make a quick edit to my silly title here, shall we?
-  document.title = "Christopher Bravo" 
-  
-  // some playful jquery shenanigans
-  $('p').hide();
-  $('p').fadeIn(1000);
-  
-  // Our birthday and today's date are always capturable in the same way so we take no arguments
-  function displayAge() {
-    var birthdate = new Date();
-    var today = new Date();
-    // today.setFullYear(2000, 0, 29);
-    birthdate.setFullYear(1999, 0, 29);
-    
-    var years;
-    //var months;
-    //var days;
-    
-    if (today.getMonth() > birthdate.getMonth()) {
-    	years = today.getYear() - birthdate.getYear();
-        // months = today.getMonth() - birthdate.getMonth();
-    } else if (today.getMonth() == birthdate.getMonth()) {
-    	if (today.getDate() >= birthdate.getDate()) {
-			years = today.getYear() - birthdate.getYear();
-        } else {
-        	years = today.getYear() - birthdate.getYear() - 1;
-        }
-        // months = 0;
-    }
-    
-    // days = Math.abs(today.getDate() - birthdate.getDate());
-    
-    document.getElementById("age").innerHTML = years;
-    // document.getElementById("age2").innerHTML = months + " month(s) ";
-    // document.getElementById("age3").innerHTML = days + " day(s) old.";
-  }
+import $ from "jquery";
 
-displayAge();
+function calcBravoAgeYears() {
+  let birthdate = new Date();
+  let today = new Date();
   
-  // maybe this works better
+  birthdate.setFullYear(1999, 0, 29);
+  birthdate.setUTCHours(2, 0, 0, 0);
+  
+  if (today.getMonth() == birthdate.getMonth() && today.getDate() < birthdate.getDate()) return today.getYear() - birthdate.getYear() - 1;
+  return today.getYear() - birthdate.getYear();
+}
+
+const bravoYears = calcBravoAgeYears();
+
+$(function() {
+  document.title = "Christopher Bravo" 
   document.getElementById("logoLink").innerHTML = "Christopher Bravo";
-}); // end of "main" / document ready function
+  document.getElementById("age").innerHTML = bravoYears;
+}); 
